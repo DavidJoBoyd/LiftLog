881097e18b830f331729c139781237be6f7f601c
DavidJoBoyd <cw.davidboyd@gmail.com>
2023-07-28 10:22:07 -0500

    Migrations

---
+++ LiftLog/Migrations/20230727181459_NextMigration2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiftLog.Migrations
+{
+    /// <inheritdoc />
+    public partial class NextMigration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkoutProgramId",
+                table: "Sets",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sets_WorkoutProgramId",
+                table: "Sets",
+                column: "WorkoutProgramId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sets_WorkoutPrograms_WorkoutProgramId",
+                table: "Sets",
+                column: "WorkoutProgramId",
+                principalTable: "WorkoutPrograms",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sets_WorkoutPrograms_WorkoutProgramId",
+                table: "Sets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sets_WorkoutProgramId",
+                table: "Sets");
+
+            migrationBuilder.DropColumn(
+                name: "WorkoutProgramId",
+                table: "Sets");
+        }
+    }
+}
---
+++ LiftLog/Migrations/20230728134012_NextMigration4.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiftLog.Migrations
+{
+    /// <inheritdoc />
+    public partial class NextMigration4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sets_WorkoutPrograms_WorkoutProgramId",
+                table: "Sets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sets_WorkoutPrograms_WorkoutProgramId",
+                table: "Sets",
+                column: "WorkoutProgramId",
+                principalTable: "WorkoutPrograms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sets_WorkoutPrograms_WorkoutProgramId",
+                table: "Sets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sets_WorkoutPrograms_WorkoutProgramId",
+                table: "Sets",
+                column: "WorkoutProgramId",
+                principalTable: "WorkoutPrograms",
+                principalColumn: "Id");
+        }
+    }
+}
